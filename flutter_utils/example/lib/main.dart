38b03b489d796b3a81e5b847d60cbeb032f65607
yangchong <yangchong211@163.com>
2021-07-18 19:16:30 +0800

    晚上网络请求工具类

--- flutter_utils/example/lib/main.dart
+++ flutter_utils/example/lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:yc_flutter_utils/utils/flutter_init_utils.dart';
 import 'package:yc_flutter_utils_example/main/main_app_page.dart';
 
 void main() {
+  //初始化工具类操作
+  Future(() async {
+    await FlutterInitUtils.fetchInitUtils();
+  });
+  //await FlutterInitUtils.fetchInitUtils();
+  //FlutterInitUtils.fetchInitUtils();
   runApp(MainApp());
 }
